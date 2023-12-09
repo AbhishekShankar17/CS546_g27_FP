25d6e7824f94fcc463f9e4d9204ee5886304ffca
Abhishek Shankar <ab17@abhisheks-air.mynetworksettings.com>
2023-12-09 13:21:35 -0500

    added folder structure

---
+++ data/users.js
@@ -0,0 +1,145 @@
+//import mongo collections, bcrypt and implement the following data functions
+import {events, users} from '../config/mongoCollections.js';
+import {ObjectId} from 'mongodb';
+import bcrypt from 'bcrypt';
+import validation from '../helpers.js';
+
+
+export const createUser = async (
+  firstName,
+  lastName,
+  emailAddress,
+  password,
+  role,
+) => {
+  if( !firstName|| !lastName|| !emailAddress|| !password|| !role){
+    throw 'Error: Must provide all fields'
+  }
+  firstName = validation.checkString(firstName, "First name");
+  if(firstName.length<2 || firstName.length>25){
+    throw 'Error: Invalid first name length'
+  }
+  lastName = validation.checkString(lastName, "Last name");
+  if(lastName.length<2 || lastName.length>25){
+    throw 'Error: Invalid last name length'
+  }
+  emailAddress = emailAddress.toLowerCase();
+  if(!( /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(emailAddress))){
+    throw 'Error: Invalid email address'
+  }
+  const userCollection= await users();
+  let user = await userCollection.findOne({emailAddress})
+  if(user){
+      throw 'there is already a user with that email address'
+    }
+
+  password = validation.checkString(password, "Password");
+  if((/^(.{0,7}|[^0-9]*|[^A-Z]*|[a-zA-Z0-9]*)$/.test(password))){
+    throw 'Error: Invalid Password'
+  }
+  if(password.match(/\s/g)){
+    throw 'Error: Invalid Password'
+  }
+  role = validation.checkString(role, "Role")
+  role = role.toLowerCase();
+  if(!(/^(admin|user)$/.test(role))){
+    throw 'Error: Invalid role'
+  }
+  const hash = await bcrypt.hash(password, 16);
+  let newUser={
+    firstName: firstName,
+    lastName: lastName,
+    emailAddress: emailAddress,
+    password: hash,
+    role: role,
+    credits:100,
+  }
+  
+  const insertInfo = await userCollection.insertOne(newUser);
+  console.log('Inserted the user into the databse successfully')
+  if (!insertInfo.acknowledged || !insertInfo.insertedId)
+    throw 'Could not add band'
+  let obj={insertedUser: true};
+  return obj;
+};
+
+export const checkUser = async (emailAddress, password) => {
+  if(!emailAddress|| !password){
+    throw 'Error: emailAddress or password not supplied'
+  }
+  if(!( /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(emailAddress))){
+    throw 'Error: Invalid email address'
+  }
+  emailAddress = emailAddress.toLowerCase();
+  password = validation.checkString(password, "Password");
+  if((/^(.{0,7}|[^0-9]*|[^A-Z]*|[a-zA-Z0-9]*)$/.test(password))){
+    throw 'Error: Invalid Password'
+  }
+  if(password.match(/\s/g)){
+    throw 'Error: Invalid Password'
+  }
+
+  const userCollection = await users();
+  let userList = await userCollection.findOne({emailAddress:emailAddress});
+  let found=false;
+  let tempuser;
+  if(userList){
+    found = true;
+    tempuser = userList;
+  }
+  // for(let i in userList){
+  //   if(userList[i].emailAddress===emailAddress){
+  //     tempuser = userList[i];
+  //     found=true;
+  //   }
+  // }
+  if(found==false){
+    throw 'Either the email address or password is invalid'
+  }
+  let compareToMatch = await bcrypt.compare(password, tempuser.password);
+  if(!compareToMatch){
+    throw 'Either the email address or password is invalid'
+  }
+  delete tempuser.password;
+  return tempuser;
+};
+
+export const createEvent = async (
+  organizer,
+  capacity,
+      date,
+      duration,
+      location,
+      time,
+      eventName,
+) => {
+  if( !organizer|| !date|| !duration|| !location|| !time|| !eventName|| !capacity){
+    throw 'Error: Must provide all fields'
+  }
+
+  const eventCollection= await events();
+  const userCollection= await users();
+  let newEvent={
+    organizer: organizer,
+    capacity: capacity,
+    date: date,
+    duration: duration,
+    location:location,
+    time:time,
+    eventName:eventName,
+  }
+  
+  const savedMeeting = await eventCollection.insertOne(newEvent);
+  // const updatedUser = await userCollection.findByIdAndUpdate(
+  //   organizer,
+  //   { $inc: { credits: -1 } }, // Assuming each meeting costs 1 credit
+  //   { new: true }
+  // );
+
+  res.status(201).json({ meeting: savedMeeting, user: updatedUser });
+console.log("Created the New Event with id ",organizer)
+  if (!insertInfo.acknowledged || !insertInfo.insertedId)
+    throw 'Could not add band'
+  let obj={insertedUser: true};
+  return obj;
+};
---
+++ routes/auth_routes.js
@@ -0,0 +1,360 @@
+//import express, express router as shown in lecture code
+
+import { createEvent, createUser } from "../data/users.js";
+import { checkUser } from "../data/users.js";
+import validation from '../helpers.js';
+
+import { Router } from "express";
+const router = Router();
+
+router.route("/").get(
+  (req, res, next) => {
+    if (req.session.user) {
+      if (req.session.user.role == "user") {
+        return res.redirect("/protected");
+      }
+      if (req.session.user.role == "admin") {
+        return res.redirect("/admin");
+      }
+    } else {
+      return res.redirect("/login");
+    }
+    next();
+  },
+  async (req, res) => {
+    //code here for GET THIS ROUTE SHOULD NEVER FIRE BECAUSE OF MIDDLEWARE #1 IN SPECS.
+    return res.json({ error: "YOU SHOULD NOT BE HERE!" });
+  }
+);
+
+router
+  .route("/register")
+  .get(async (req, res) => {
+    //code here for GET
+    res.render("register");
+  })
+  .post(async (req, res) => {
+    //code here for POST
+    try {
+      let firstNameInput;
+      let lastNameInput;
+      let emailAddressInput;
+      let passwordInput;
+      let confirmPasswordInput;
+      let roleInput;
+      
+      if(req.body){
+      firstNameInput= req.body.firstNameInput;
+      lastNameInput= req.body.lastNameInput;
+      emailAddressInput= req.body.emailAddressInput;
+      passwordInput= req.body.passwordInput;
+      confirmPasswordInput= req.body.confirmPasswordInput;
+      roleInput= req.body.roleInput;
+      }
+
+      if (
+        !firstNameInput ||
+        !lastNameInput ||
+        !emailAddressInput ||
+        !passwordInput ||
+        !roleInput ||
+        !confirmPasswordInput
+      ) {
+        throw "Error: Must provide all fields";
+      }
+      firstNameInput = validation.checkString(
+        firstNameInput,
+        "First name"
+      );
+      if (firstNameInput.length < 2 || firstNameInput.length > 25) {
+        throw "Error: Invalid first name length";
+      }
+      lastNameInput = validation.checkString(
+        lastNameInput,
+        "Last name"
+      );
+      if (lastNameInput.length < 2 || lastNameInput.length > 25) {
+        throw "Error: Invalid last name length";
+      }
+      emailAddressInput = emailAddressInput.toLowerCase();
+      if (
+        !/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+          emailAddressInput
+        )
+      ) {
+        throw "Error: Invalid email address";
+      }
+
+      passwordInput = validation.checkString(
+        passwordInput,
+        "Password"
+      );
+      if (/^(.{0,7}|[^0-9]*|[^A-Z]*|[a-zA-Z0-9]*)$/.test(passwordInput)) {
+        throw "Error: Invalid Passwords";
+      }
+      if (passwordInput.match(/\s/g)) {
+        throw "Error: Invalid Passwords";
+      }
+      if (confirmPasswordInput !== passwordInput) {
+        throw "Error: Passwords do not match";
+      }
+      roleInput = validation.checkString(roleInput, "Role");
+      if (!/^(admin|user)$/.test(roleInput)) {
+        throw "Error: Invalid role";
+      }
+    } catch (e) {
+      return res.status(400).render("register", { error: `${e}`});
+    }
+    try {
+
+      const newuser = await createUser(
+        req.body.firstNameInput,
+        req.body.lastNameInput,
+        req.body.emailAddressInput,
+        req.body.passwordInput,
+        req.body.roleInput
+      );
+
+      let out = { insertedUser: true };
+      if (
+        Object.entries(newuser).toString() == Object.entries(out).toString()
+      ) {
+        return res.redirect("/login");
+      } else {
+        throw "Internal Server Error";
+      }
+    } catch (e) {
+      return res.status(500).render("register", { error: `${e}` });
+    }
+  });
+
+router.route("/login")
+  .get(async (req, res) => {
+    //code here for GET
+    res.render("login");
+  })
+  .post(async (req, res) => {
+    //code here for POST
+    try {
+      let {emailAddressInput, passwordInput} = req.body;
+      if (!emailAddressInput || !passwordInput) {
+        throw "Error: Must provide both email address and password";
+      }
+      emailAddressInput = emailAddressInput.toLowerCase();
+      if (
+        !/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+          emailAddressInput
+        )
+      ) {
+        throw "Error: Invalid email address";
+      }
+
+      passwordInput = validation.checkString(
+        passwordInput,
+        "Password"
+      );
+      if (
+        /^(.{0,7}|[^0-9]*|[^A-Z]*|[a-zA-Z0-9]*)$/.test(passwordInput)
+      ) {
+        throw "Error: Invalid Password";
+      }
+      if (passwordInput.match(/\s/g)) {
+        throw "Error: Invalid Password";
+      }
+    } catch (e) {
+      return res.status(400).render("login", { error: `${e}`});
+    }
+    try {
+      let auth_user = await checkUser(
+        req.body.emailAddressInput,
+        req.body.passwordInput
+      );
+      if (auth_user) {
+        req.session.user = auth_user;
+      }
+      if(req.session.user){
+        if (req.session.user.role == "admin") {
+        return res.redirect("/admin");
+      }
+        if (req.session.user.role == "user") {
+        return res.redirect("/protected");
+      }
+    }
+    } catch (e) {
+      return res.status(400).render("login", { error:`${e}`});
+    }
+  });
+
+router.route("/protected").get(async (req, res) => {
+  //code here for GET
+  let admin = false;
+  
+  if(req.session.user){
+  try{
+    let today = new Date();
+    let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    if (req.session.user.role === "admin") {
+      admin = true;
+      res.render("protected", {
+        firstName: req.session.user.firstName,
+        currentTime: time,
+        role: req.session.user.role,
+        admin: admin,
+    });
+  }
+  }catch(e){
+    return res.status(500).render('error', {error:`${e}`});
+  }
+  let today = new Date();
+  let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  res.render("protected", {
+    firstName: req.session.user.firstName,
+    currentTime: time,
+    role: req.session.user.role
+  });
+  }
+});
+
+router.route("/admin").get(async (req, res) => {
+  //code here for GET
+  let today = new Date();
+  let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+  res.render("admin",{firstName: req.session.user.firstName, currentTime:time, admin:true});
+});
+
+router.route("/error").get(async (req, res) => {
+  //code here for GET
+  res.status(403).render("error");
+});
+
+router.route("/logout").get(async (req, res) => {
+  //code here for GET
+  req.session.destroy();
+  res.render("logout");
+});
+
+router.route("/createEvent")
+  .get(async (req, res) => {
+    //code here for GET
+    res.render("createEvent");
+  })
+  .post(async (req, res) => {
+    //code here for POST
+    try {
+      // const {organizer, capacity, date, duration,location,time,eventName } = req.body;
+      // const newMeeting = new Meeting({
+      //         organizer,
+      //         capacity,
+      //         room,
+      //         date,
+      //         duration,
+      //         location,
+      //         time,
+      //         eventName,
+      //         // Add other meeting properties as needed
+      //       });
+        
+            const savedMeeting = await createEvent(
+              req.body.organizer,
+              req.body.capacity,
+              req.body.date,
+              req.body.duration,
+              req.body.location,
+              req.body.time,
+              req.body.eventName,
+            );
+        
+            // Deduct credits from the organizer
+            const updatedUser = await User.findByIdAndUpdate(
+              organizer,
+              { $inc: { credits: -1 } }, // Assuming each meeting costs 1 credit
+              { new: true }
+            );
+        
+            res.status(201).json({ meeting: savedMeeting, user: updatedUser });
+    } catch (e) {
+      return res.status(400).render("createEvent", { error: `${e}`});
+    }
+  });
+
+// router.route("/createEvent").get(async (req, res) => {
+//   res.render("createEvent");
+// }).
+// post(async (req, res) => {
+//   try {
+//     const { room, date, duration,location,time,eventName } = req.body;
+//     const organizer = req.user.id; // Assuming you use authentication middleware
+
+//     const newMeeting = new Meeting({
+//       organizer,
+//       capacity,
+//       room,
+//       date,
+//       duration,
+//       location,
+//       time,
+//       eventName,
+//       // Add other meeting properties as needed
+//     });
+
+//     const savedMeeting = await createEvent(
+//       req.body.organizer,
+//       req.body.capacity,
+//       req.body.date,
+//       req.body.duration,
+//       req.body.location,
+//       req.body.time,
+//       req.body.eventName,
+//     );
+
+//     // Deduct credits from the organizer
+//     const updatedUser = await User.findByIdAndUpdate(
+//       organizer,
+//       { $inc: { credits: -1 } }, // Assuming each meeting costs 1 credit
+//       { new: true }
+//     );
+
+//     res.status(201).json({ meeting: savedMeeting, user: updatedUser });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: 'Internal Server Error' });
+//   }
+// });
+
+// Delete Meeting
+router.delete('/:id', async (req, res) => {
+  try {
+    const meetingId = req.params.id;
+    const organizerId = req.user.id; // Assuming you use authentication middleware
+
+    const meeting = await Meeting.findById(meetingId);
+
+    if (!meeting) {
+      return res.status(404).json({ error: 'Meeting not found' });
+    }
+
+    if (String(meeting.organizer) !== organizerId) {
+      return res.status(403).json({ error: 'Unauthorized' });
+    }
+
+    // Check if the meeting is scheduled in the future, you might want to use a specific field for this
+    if (meeting.date > new Date()) {
+      await Meeting.findByIdAndDelete(meetingId);
+
+      // Refund credits to the organizer
+      const updatedUser = await User.findByIdAndUpdate(
+        organizerId,
+        { $inc: { credits: 1 } }, // Refund 1 credit
+        { new: true }
+      );
+
+      res.json({ message: 'Meeting deleted successfully', user: updatedUser });
+    } else {
+      res.status(400).json({ error: 'Cannot delete past meetings' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+export default router;
