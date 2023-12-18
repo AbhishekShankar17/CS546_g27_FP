9be1b60b6fd81007aff242fc8f1d60f04289220c
Abhishek Shankar <147009256+AbhishekShankar17@users.noreply.github.com>
2023-12-17 23:45:05 -0500

    Update seeds.js

--- seeds.js
+++ seeds.js
@@ -49,6 +49,161 @@ import * as usersData from './data/users.js';
 // }catch(e){
 //     console.log(e);
 // }
+try {
+  const newUser = await usersData.createUser(
+    'John',
+    'Doe',
+    'john.doe@stevens.edu',
+    'Password123!',
+    'user'
+  );
+  console.log('New user created:', newUser);
+} catch (error) {
+  console.error('Error creating a new user:', error);
+}
+
+
+try {
+  const newUser = await usersData.createUser('', 'Doe', '', 'Password123!', 'user');
+  console.log('New user created:', newUser);
+} catch (error) {
+  console.error('Error creating a new user:', error);
+}
+
+
+try {
+  const user = await usersData.checkUser('john.doe@example.com', 'Password123!');
+  console.log('User credentials checked:', user);
+} catch (error) {
+  console.error('Error checking user credentials:', error);
+}
+
+
+try {
+  const user = await usersData.checkUser('john.doe@example.com', 'InvalidPassword');
+  console.log('User credentials checked:', user);
+} catch (error) {
+  console.error('Error checking user credentials:', error);
+}
+
+
+try {
+  const newEvent = await usersData.createEvent(
+    50,
+    '2023-12-20',
+    2,
+    'Babbio',
+    '19:30',
+    'Sample Event',
+    'john.doe@example.com'
+  );
+  console.log('New event created:', newEvent);
+} catch (error) {
+  console.error('Error creating a new event:', error);
+}
+
+
+try {
+  const newEvent = await usersData.createEvent(
+    50,
+    '2023-12-10', 
+    2,
+    'Babbio',
+    '19:30',
+    'Sample Event',
+    'john.doe@example.com'
+  );
+  console.log('New event created:', newEvent);
+} catch (error) {
+  console.error('Error creating a new event:', error);
+}
+
+
+try {
+  const transferResult = await usersData.creditsTransfer(
+    'john.doe@example.com',
+    'receiver@example.com',
+    '10',
+    'john.doe@example.com'
+  );
+  console.log('Credits transferred successfully:', transferResult);
+} catch (error) {
+  console.error('Error transferring credits:', error);
+}
+
+
+try {
+  const transferResult = await usersData.creditsTransfer(
+    'john.doe@example.com',
+    'receiver@example.com',
+    '1000',
+    'john.doe@example.com'
+  );
+  console.log('Credits transferred successfully:', transferResult);
+} catch (error) {
+  console.error('Error transferring credits:', error);
+}
+
+// Add more test cases for creditsTransfer...
+
+// Test case 9: Get all events
+try {
+  const allEvents = await usersData.getallevents();
+  console.log('All events:', allEvents);
+} catch (error) {
+  console.error('Error fetching all events:', error);
+}
+
+// Test case 10: Get event by name
+try {
+  const eventName = 'Sample Event';
+  const event = await usersData.getEventByName(eventName);
+  console.log(`Event with name '${eventName}':`, event);
+} catch (error) {
+  console.error(`Error fetching event with name '${eventName}':`, error);
+}
+
+// Add more test cases for other functions...
+
+// Test case 11: Event registration
+try {
+  const registrationResult = await usersData.eventRegistration(
+    'Sample Event',
+    'Babbio',
+    '2023-12-20',
+    '19:30',
+    { session: { user: { emailAddress: 'john.doe@example.com' } } }
+  );
+  console.log('Event registration result:', registrationResult);
+} catch (error) {
+  console.error('Error registering for an event:', error);
+}
+
+// Test case 12: Event registration with invalid event details
+try {
+  const registrationResult = await usersData.eventRegistration(
+    'Invalid Event',
+    'Babbio',
+    '2023-12-20',
+    '19:30',
+    { session: { user: { emailAddress: 'john.doe@example.com' } } }
+  );
+  console.log('Event registration result:', registrationResult);
+} catch (error) {
+  console.error('Error registering for an event:', error);
+}
+
+// Add more test cases for eventRegistration...
+
+// Test case 13: Deleting an event
+try {
+  const eventIdToDelete = 'your_event_id_here';
+  const deletionResult = await usersData.deleteEvent(eventIdToDelete);
+  console.log('Event deletion result:', deletionResult);
+} catch (error) {
+  console.error('Error deleting an event:', error);
+}
+
 
 try{
     console.log(await usersData.deleteEvent("cpt","Babbio","10:30 PM","12/19/2023"));
